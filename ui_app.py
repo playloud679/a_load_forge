685623846b053f64b01ff43a98a0e8bbba1f1a8a
marco_d <playloud79@gmail.com>
2026-07-24 14:56:31 +0200

    style: move sidebar logo even higher to -4.0rem

--- ui_app.py
+++ ui_app.py
@@ -122,7 +122,7 @@ st.markdown(
     }
     section[data-testid="stSidebar"] [data-testid="stSidebarUserContent"] {
         padding-top: 0 !important;
-        margin-top: -2.8rem !important;
+        margin-top: -4.0rem !important;
     }
     section[data-testid="stSidebar"] [data-testid="stMarkdownContainer"] h3 {
         border-top: 1px solid rgba(255,255,255,.10);
